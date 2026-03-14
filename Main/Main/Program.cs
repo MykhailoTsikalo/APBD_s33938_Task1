733c981dacdcc1dffbafc770cdd8ab6ef3568a36
Mykhailo Tsikalo <s33938@pjwstk.edu.pl>
2026-03-14 16:27:56 +0100

    Update welcome message to THE APP instead of THE STATISTICS APP

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -7,7 +7,7 @@ namespace Main
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Statistics App!");
+            Console.WriteLine("Welcome to the App!");
             Console.WriteLine("Please enter a number:");
             string input = Console.ReadLine();
 
