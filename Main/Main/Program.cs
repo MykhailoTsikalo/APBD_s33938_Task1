79cc559b55b1ff706f3b8180d7e2d337794f317c
Mykhailo Tsikalo <s33938@pjwstk.edu.pl>
2026-03-14 14:49:42 +0100

    Add basic user input haldling in main program

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Statistics App!");
+            Console.WriteLine("Please enter a number:");
+            string input = Console.ReadLine();
+        }
+    }
+}
